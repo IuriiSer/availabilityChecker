dcbdf2b9edfd425575a40a5ec0e39a7de35f8162
IuriiSer <yusergeevi4@gmail.com>
2022-10-09 23:40:10 +0300

    added new services

--- src/app.ts
+++ src/app.ts
@@ -2,14 +2,20 @@ import EventEmitter from 'events';
 import { EventTypes } from './models/EventTypes';
 import LogService from './services/LogService';
 import UrlService from './services/UrlService';
+import CheckService from './services/CheckService';
+import { Url } from './models/UrlTypes';
+import { CsvService } from './services/CsvService';
+
 
 async function app() {
 	try {
 		const events = new EventEmitter();
 		const urlService = new UrlService(events);
+		const checkService = new CheckService(events);
 		const logService = new LogService();
+		const csvService = new CsvService(events);
 		let isErr = false;
-
+		events.setMaxListeners(20);
 		events.on(EventTypes.Log, (message: string) => {
 			if (isErr) return;
 			logService.addTask({ status: EventTypes.Ok, message });
@@ -17,29 +23,40 @@ async function app() {
 
 		events.on(EventTypes.Error, (message: string, err?: string) => {
 			if (isErr) return;
-      isErr = true;
 			logService.addTask({ status: EventTypes.Error, message, err });
 		});
 
-		events.on(EventTypes.WorngUrl, (url: string) => {
+		events.on(EventTypes.ErrorFatal, (message: string, err?: string) => {
+			if (isErr) return;
+			isErr = true;
+			logService.addTask({ status: EventTypes.ErrorFatal, message, err });
+		});
+
+		events.on(EventTypes.WorngUrl, (url: Url) => {
 			if (isErr) return;
 			const message = `Wrong format for URL -> '${url}'`;
 			logService.addTask({ status: EventTypes.Warning, message });
 		});
 
-		events.on(EventTypes.NotUrl, (url: string) => {
+		events.on(EventTypes.NotUrl, (url: Url) => {
 			if (isErr) return;
 			const message = `URL looks like a not real url -> '${url}'. It will be skipped.`;
 			logService.addTask({ status: EventTypes.Warning, message });
 		});
 
-		events.on(EventTypes.UrlInCheck, (url: string) => {
+		events.on(EventTypes.UrlAlreadyInCheck, (url: Url) => {
 			if (isErr) return;
 			const message = `URL '${url}' is already il list for checks. It will be skipped.`;
 			logService.addTask({ status: EventTypes.Warning, message });
 		});
 
+		events.on(EventTypes.CheckUrl, (url: Url) => {
+			if (isErr) return;
+			checkService.addTask(url);
+		});
+
 		await logService.init();
+		await csvService.init();
 		urlService.getUrlsToCheck();
 	} catch (e) {
 		console.error(e);
