ed5eca307f59f93fde60f1fb16ec21632aa77e2b
IuriiSer <yusergeevi4@gmail.com>
2022-10-10 16:51:39 +0300

    rebuild url service to async

--- src/services/UrlService.ts
+++ src/services/UrlService.ts
@@ -6,57 +6,45 @@ import { Urls } from '../models/UrlTypes';
 
 const prisma = new PrismaClient();
 
+// UrlService can get urls from db
+// check it
+// add notify throw eHandler about new events
 class UrlService {
-	private _urls: Urls = [];
+	private urls: Urls = [];
 	private eHandler: EventEmitter;
+	private _isInWork: boolean = false;
 
 	constructor(_eHandler: EventEmitter) {
 		this.eHandler = _eHandler;
 	}
 
-	public get urls(): Urls {
-		return this._urls;
+	get isInWork(): boolean {
+		return this._isInWork;
 	}
 
 	public async getUrlsToCheck(): Promise<Urls | null> {
 		try {
+			this._isInWork = true;
 			this.eHandler.emit(EventTypes.Log, 'Start getting URLS from the data base');
 			const rawUrls = await prisma.urlToCheck.findMany();
 			if (!Array.isArray(rawUrls))
 				throw new Error('Something with the data base. URLS should be an array');
 
-			rawUrls.forEach((rawUrl) => {
-				const { url } = rawUrl;
+			await Promise.all(
+				rawUrls.map((rawUrl) => {
+					const { url } = rawUrl;
+					this.eHandler.emit(EventTypes.Log, `Get ${url} for checking`);
+					return this.validateUrl(url);
+				}),
+			);
 
-				this.eHandler.emit(EventTypes.Log, `Url - ${url} -> Validating`);
-
-				if (typeof url !== 'string') {
-					this.eHandler.emit(EventTypes.WorngUrl, url);
-					return;
-				}
-
-				const formatedUrl = validUrl.isUri(url);
-				if (!formatedUrl) {
-					this.eHandler.emit(EventTypes.NotUrl, url);
-					return;
-				}
-
-				if (this._urls.includes(formatedUrl)) {
-					this.eHandler.emit(EventTypes.UrlAlreadyInCheck, url);
-					return;
-				}
-
-				this.eHandler.emit(EventTypes.Log, `Url - ${url} -> OK`);
-				this.eHandler.emit(EventTypes.CheckUrl, url);
-
-				this._urls.push(url);
-			});
+			this._isInWork = false;
 
 			this.eHandler.emit(
 				EventTypes.Log,
-				`Done to add URL that will be checked for availability, added ${this._urls.length} urls`,
+				`Done to add URL that will be checked for availability, added ${this.urls.length} urls`,
 			);
-
+			this.eHandler.emit(EventTypes.Finish);
 			return this.urls;
 		} catch (err) {
 			const message = 'Something went wrong during getting urls to check. Exit programm.';
@@ -67,6 +55,40 @@ class UrlService {
 			this.eHandler.emit(EventTypes.ErrorFatal, message);
 			return null;
 		}
+	}
+
+	private validateUrl(url: string): Promise<void> {
+		return new Promise((res) => {
+			setTimeout(() => {
+				this.eHandler.emit(EventTypes.Log, `Url - ${url} -> Validating`);
+
+				if (typeof url !== 'string') {
+					this.eHandler.emit(EventTypes.WorngUrl, url);
+					res();
+					return;
+				}
+
+				const formatedUrl = validUrl.isUri(url);
+				if (!formatedUrl) {
+					this.eHandler.emit(EventTypes.NotUrl, url);
+					res();
+					return;
+				}
+
+				if (this.urls.includes(formatedUrl)) {
+					this.eHandler.emit(EventTypes.UrlAlreadyInCheck, url);
+					res();
+					return;
+				}
+
+				this.eHandler.emit(EventTypes.Log, `Url - ${url} -> OK`);
+				this.eHandler.emit(EventTypes.CheckUrl, url);
+
+				this.urls.push(url);
+
+				res();
+			}, 0);
+		});
 	}
 }
 
